f576906a694b6255bf21a2950acb954e1f07f61a
Christopher Lamlamay <Christopher@ChristophersMBP.lan>
2017-01-23 08:54:16 -0600

    updaed README

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# oragami
+
+Multiplication by 'Oragami Games'
+(Oragami Games is a ficticious name)
+
+Contributors:
+
+Lead HTML/CSS - Christopher Lamlamay
+Lead Javascript - Traci Williams
+
+### About: 
+
+This was our first group project at General Assembly. This was the second week of class after we fully immersed in HTML/CSS/JAVASCRIPT/JQUERY.
+
+Traci and myself have a passion for Education from teaching to policy reform. We wanted to create a game in the short 24 hours we were given.
+
+Practice your third grade math skills with our simple multiplication game.
+
+### Technologies:
+
+- HTML
+- CSS
+- jQuery
+- JavaScript
+
+### Approach: 
+
+1) Game idea sketches, extremely simple wireframes.
+2) Collaborate on the HTML structure.
+3) Divide HTML/CSS (Christopher) and Javascript (Traci).
+4) Merge and Test Code.
+5) Deploy on Github/Heroku when completed.
+
+### Update:
+
+The game has been updated from Javascript to jQuery. 
+Less code and easy on the eyes.
+
+This game made me realize that I still count on my fingers.
+
+
--- scripts/app.js
+++ scripts/app.js
@@ -3,18 +3,17 @@
     var counter = setInterval(timer, 1000); 
     var score = 0;
 
-function getScore() {
-    $( "#score" ).text("Current score:  " + score);
-  };
-getScore();
-
-
 function getName() {
     var username = prompt('What is your name?');
     $("#name").text( "Welcome, " + username + "!");
     alert('Time to multiply!');
   };
 getName();
+
+function getScore() {
+    $( "#score" ).text("Current score:  " + score);
+  };
+getScore();
 
 function timer(){
     count = count-1;
@@ -26,7 +25,6 @@ function timer(){
   };
 timer();
 
-
 function getMathQuestion(){
     var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
     var randomValues = [];
@@ -35,58 +33,35 @@ function getMathQuestion(){
     var valueOne = numbers[Math.floor(Math.random() * numbers.length)];
     var valueTwo = numbers[Math.floor(Math.random() * numbers.length)];
     var answer = valueOne * valueTwo;
-    var button = $('button');
-    
-    randomAnswers.push(answer)
-    console.log(valueOne);
-    console.log(valueTwo);
-    console.log('What is ' + valueOne + ' x ' + valueTwo + '?');
-    console.log('This is the Answer: ' + valueOne * valueTwo);
+    randomAnswers.push(answer);
     $("#question").text('What is ' + valueOne + ' x ' + valueTwo + '?');
-
     var randomNumberOne = Math.floor(Math.random() * 100);
-    console.log(randomNumberOne)
     randomValues.push(randomNumberOne);
-
     var randomNumberTwo = Math.floor(Math.random() * 100);
-    console.log(randomNumberTwo)
     randomValues.push(randomNumberTwo);  
-
     var randomNumberThree = Math.floor(Math.random() * 100);
-    console.log(randomNumberThree)
     randomValues.push(randomNumberThree);    
-
     var gameAnswers = randomValues.concat(randomAnswers);
-    console.log(gameAnswers);
     gameAnswers.sort(function(a, b){return 0.5 - Math.random()});
-    console.log(gameAnswers[0]);
-    console.log(gameAnswers[1]);
-    console.log(gameAnswers[2]);
-    console.log(gameAnswers[3]);
-
     $('button#one').text(gameAnswers[0]);
     $('button#two').text(gameAnswers[1]);
     $('button#three').text(gameAnswers[2]);
-    $('button#four').text(gameAnswers[3]);
-
-    console.log("This is the final answer: " + answer);
-    
+    $('button#four').text(gameAnswers[3]);    
     $('button').on("click", function(){
-      console.log(this.innerHTML);
-      console.log(typeof this);
-      console.log(typeof answer);
-      console.log(this.innerHTML == answer);
       if (this.innerHTML == answer) {
-        alert("Correct!");
         score = score + 5;
         $("#score").text("Score: " + score);
+        alert("Correct!");
         getMathQuestion();
       } else {
-        alert("Try Again.")
-      }
+        alert("Try Again.");
+      };
     });
   };
+
 getMathQuestion();
+
+
 
 
 
